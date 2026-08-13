36c592af883fb0916dabacb51b93c55205a48665
Akther Jahan Mim <236569494+Akther-Jahan@users.noreply.github.com>
2026-08-13 19:55:02 +0600

    Add live overview component

---
+++ src/features/admin/components/LiveOverview.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function LiveOverview() {
+  return (
+    <View>
+      <Text>Live overview</Text>
+    </View>
+  );
+}
